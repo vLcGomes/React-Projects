b917c4d90e9dfc8684131023217805cc2e5d9b4a
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-04-22 21:13:03 -0300

    Ajustes

--- carrinhocompras/src/App.js
+++ carrinhocompras/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import Display from './components/Display';
+import Button from './components/Button';
+import Cart from './components/Cart'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='content'>
+
+      </div>
     </div>
   );
 }
